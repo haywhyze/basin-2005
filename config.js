147c9f804abb1f92e35ade8ac89ce5b89b21c4a2
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-01-07 13:30:49 +0100

    fix github debugging issues

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteTitle: 'USS 2010 - Student Gallery', // <title>
   manifestName: 'USS 2010',
-  manifestShortName: 'USS \'10', // max 12 characters
+  manifestShortName: "USS '10", // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#AD67FC',
   manifestThemeColor: '#AD67FC',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-multiverse/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: '/uss10/', // This path is subpath of your hosting https://domain/portfolio
   heading: 'USS 2010',
-  subHeading: 'Student\'s Gallery',
+  subHeading: "Student's Gallery",
   // social
   socialLinks: [
     {
